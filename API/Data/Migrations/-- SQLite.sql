37a1895a093c71b338525932ec20593c7d14216f
vanashripatil <vanashri1996@gmail.com>
2021-06-26 18:22:01 +0530

    End of section 2

---
+++ API/Data/Migrations/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Vanashri");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "James");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Min");
